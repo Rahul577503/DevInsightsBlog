85cb507bca8de3c464ca24523b52e7feee9d6adc
Rahul577503 <rahulmaurya109626@gmail.com>
2024-04-29 19:00:30 +0530

    changes index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
 import PostCard from "../components/PostCard";
-import { Spotlight } from "@/components/ui/Spotlight";
 import { TypewriterEffect } from "@/components/ui/typewrite-effect";
+import Newsletter from "@/components/Newsletter";
+import { useState } from "react";
+import Pagination from "@/components/ui/Pagination";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 interface FrontMatter {
   title: string;
@@ -32,33 +35,54 @@ const words = [
   },
   {
     text: "DevInsightsBlog",
-    className: "text-blue-500 text-base sm:text-xl md:text-2xl lg:text-3xl font-bold text-center my-20",
+    className:
+      "text-blue-500 text-base sm:text-xl md:text-2xl lg:text-3xl font-bold text-center my-20",
   },
 ];
 
 export default function Home(props: HomeProps): JSX.Element {
+  const perPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPosts = props.posts.length;
+  const totalPages = Math.ceil(totalPosts / perPage);
+
+  const startIndex = (currentPage - 1) * perPage;
+  const endIndex = currentPage * perPage;
+
+  const currentPosts = props.posts.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
   return (
-    <div className="min-h-screen w-full py-10 px-4 rounded-md flex md:items-center md:justify-center flex-col  relative overflow-hidden">
+    <div className="min-h-screen  rounded-md flex flex-col relative top-10 overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-
-      <TypewriterEffect words={words} />
-
-      {props.posts.length > 0 ? (
-        <div className="md:grid md:grid-cols-3 lg:gap-8 gap-2 px-8">
-          {props.posts.map((post, index) => (
-            <Link href={`/posts/${post.slug}`} key={index}>
-              <PostCard post={post} />
-            </Link>
-          ))}
-        </div>
-      ) : (
-        <h2 className="font-sans text-3xl">No posts yet</h2>
-      )}
-      
-      <Spotlight />
+      <div className="max-w-[50rem] mx-auto  px-4">
+        <TypewriterEffect words={words} />
+        <Newsletter />
+        <br />
+        {currentPosts.length > 0 ? (
+          <>
+            <div className="flex flex-col lg:gap-8 gap-2">
+              {currentPosts.map((post, index) => (
+                <Link href={`/posts/${post.slug}`} key={index}>
+                  <PostCard post={post} />
+                </Link>
+              ))}
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
+          </>
+        ) : (
+          <h2 className="font-sans text-3xl">No posts yet</h2>
+        )}
+      </div>
     </div>
   );
 }
@@ -72,7 +96,13 @@ export async function getStaticProps(): Promise<{ props: HomeProps }> {
     const { title, description, thumbnail, date, readTime } =
       data as FrontMatter;
     return {
-      frontMatter: { title, description, thumbnail, date, readTime: readTime || 0 }, 
+      frontMatter: {
+        title,
+        description,
+        thumbnail,
+        date,
+        readTime: readTime || 0,
+      },
       slug: file.split(".")[0],
     };
   });
